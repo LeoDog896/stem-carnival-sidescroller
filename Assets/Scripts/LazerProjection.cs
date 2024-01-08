7ad63513125cf8acbf01a2897b417a0fe361d9c4
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-08 17:30:56 -0500

    feat: base lazer

--- Assets/Scripts/LazerProjection.cs
+++ Assets/Scripts/LazerProjection.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class LazerProjection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private LineRenderer lineRenderer;
 
     // Update is called once per frame
     void Update()
     {
-        
+        var hit = Physics2D.Raycast(transform.position, -transform.up);
+
+        if (hit.collider != null)
+        {
+            // If the raycast hits something, draw a line from this object's position to the hit point
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            // If the raycast doesn't hit anything, draw a line from this object's position to 100 units in front of it
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position + transform.forward * 100);
+        }
     }
 }
