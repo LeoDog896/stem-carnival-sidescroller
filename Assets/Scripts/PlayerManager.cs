e82ba308ebf2e06a0493e13826e92edd9afcc373
Tristan F <LeoDog896@hotmail.com>
2024-01-24 09:50:27 -0500

    feat: player1/player2 split

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,13 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
 
-    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject player1Prefab;
+    [SerializeField] private GameObject player2Prefab;
     [SerializeField] private CameraHandler cameraHandler;
 
     void Start()
     {
-        var player1 = PlayerInput.Instantiate(playerPrefab, controlScheme: "KeyboardWASD", pairWithDevice: Keyboard.current);
-        var player2 = PlayerInput.Instantiate(playerPrefab, controlScheme: "KeyboardArrows", pairWithDevice: Keyboard.current);
+        var player1 = PlayerInput.Instantiate(player1Prefab, controlScheme: "KeyboardWASD", pairWithDevice: Keyboard.current);
+        var player2 = PlayerInput.Instantiate(player2Prefab, controlScheme: "KeyboardArrows", pairWithDevice: Keyboard.current);
 
         cameraHandler.players = new PlayerInput[2] { player1, player2 };
     }
